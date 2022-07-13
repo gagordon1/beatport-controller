c1e9542d82a3e0b25fd24d3a989348bdd9dd5e37
Garrett Gordon <gagordon@mit.edu>
2022-07-13 19:04:22 -0400

    modularized puppeteer process

--- controller.js
+++ controller.js
@@ -1,10 +1,66 @@
 const puppeteer = require('puppeteer');
+const WAIT_TIME = 3000
+const DEV = true
 
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-const WAIT_TIME = 1500
+async function login (p, un, pw){
+	console.log("logging in...")
+	await p.goto("https://www.beatport.com/account/login", {waitUntil: 'load'});
+	await p.$eval('input#username', (el,un) => {return el.value=un}, un);
+	await p.$eval('input#password', (el,pw) => {return el.value=pw}, pw);
+
+	await clearCookieMenu(p)
+	await p.$eval('button.login-page-form-button', button => button.click())
+	
+	await p.waitForNavigation()
+	if(DEV){
+		await p.screenshot({path: `login.png`});
+	}
+	return
+}
+
+async function searchTrack(s, p){
+	console.log(`searching for track: ${s.track} by ${s.artists}...`)
+	let searchString = (s.track + " " + s.artists).replace(/\s/g, "+")
+	let url = `https://www.beatport.com/search/tracks?q=${searchString}`
+	await p.goto(url,{waitUntil: 'load'});
+
+	if(DEV){
+		await p.screenshot({path: `screenshot${s.track}.png`});
+	}
+	await sleep(WAIT_TIME)
+	return
+}
+
+async function addFirstResultToCart(s,p){
+	await p.$$eval("a.icon.icon-ellipsis.track-ellipsis.mobile-action", elements => elements[0].click())
+
+	console.log("	clicking cart menu...")
+	await p.$eval("div.icon", icon => icon.click());
+
+	await sleep(WAIT_TIME)
+	console.log("	clicking main cart...")
+	if (DEV){
+		await p.screenshot({path: `cart-${s.track}.png`});
+	}
+	
+
+	await p.$$eval(`li[data-name^="cart"]`, e => {
+		e[e.length-1].click() //there are two on the page for some reason - should investigate further
+	})
+	await sleep(WAIT_TIME)
+	return
+}
+
+async function clearCookieMenu (p) {
+	if (await p.$("button#CybotCookiebotDialogBodyButtonDecline") != null){
+		await p.$eval( "button#CybotCookiebotDialogBodyButtonDecline", button => button.click() );
+	}
+}
+
 
 module.exports = {
 
@@ -15,67 +71,37 @@ module.exports = {
 		username : String
 		password : String
 		searches : array of searches for beatport
+		progress : {toProcess : int}
 
 		returns String[]
 	**/
-	addToBeatportCart : async (un, pw, searches) => {
+	addToBeatportCart : async (username, password, searches, progress) => {
 		const browser = await puppeteer.launch();
 		const page = await browser.newPage();
 
 		const badSearches = [];
-
-		const clearCookieMenu = async (p) => {
-			if (await p.$("button#CybotCookiebotDialogBodyButtonDecline") != null){
-				await p.$eval( "button#CybotCookiebotDialogBodyButtonDecline", button => button.click() );
-			}
-		}
+		
 		//login
-		console.log("logging in...")
-		await page.goto("https://www.beatport.com/account/login");
-		await page.$eval('input#username', (el,un) => {return el.value=un}, un);
-		await page.$eval('input#password', (el,pw) => {return el.value=pw}, pw);
-		await page.screenshot({path: 'screenshot2.png'});
-		await page.$eval('button.login-page-form-button', button => button.click())
-		await page.waitForNavigation()
-
+		
+		await login(page, username, password)
 		await clearCookieMenu(page)
 		//search a track
 		var search;
 		for (i = 0; i <searches.length; i++){
 			search = searches[i]
+
 			try{
+				await searchTrack(search, page)
+				await addFirstResultToCart(search, page)
 				
-				console.log(`searching for track: ${search}...`)
-				await page.goto('https://www.beatport.com/search/tracks?q='+search,{waitUntil: 'load', timeout: 0});
-
-				
-				await page.screenshot({path: `screenshot${search}.png`});
-				await page.$$eval("a.icon.icon-ellipsis.track-ellipsis.mobile-action", elements => elements[0].click())
-
-				// await page.screenshot({path: 'screenshot5.png'});
-
-				console.log("clicking cart menu...")
-				await page.$eval("div.icon", icon => icon.click());
-
-				await sleep(WAIT_TIME)
-				console.log("trying to click main cart...")
-				// await page.screenshot({path: 'screenshot6.png'});
-
-				await page.$$eval(`li[data-name^="cart"]`, e => {
-					e[e.length-1].click() //there are two on the page for some reason - should investigate further
-				})
-				await sleep(WAIT_TIME)
-
-				await clearCookieMenu(page)
-				await page.screenshot({path: 'screenshot7.png'});
 			}catch(error){
 				console.log(error)
 				badSearches.push(search)
+				progress.badSearches = badSearches
 			}
-
+			progress.toProcess--;
 			
 		}
-		
     	browser.close();
 		return badSearches
 	}
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
 
-
+const SECONDS_IN_A_DAY = 86400
 const DEVELOPMENT = true;
 const port = 8080
 const DEVELOPMENT_FRONTEND = "http://localhost:3000"
 const PRODUCTION_FRONTEND = ""
 const controller = require("./controller.js")
+const { v4: uuidv4 } = require('uuid');
+
+const STATUS_TIMEOUT = 1000 * 1000 //milliseconds in 1000 seconds
+const CLEANUP_INTERVAL =   60 * 1000 // milliseconds in a day
+
+const status = {}
+
+function cleanUpStatus(){
+	const ids = Object.keys(status);
+	ids.forEach(id =>{
+		let progress = status[id]
+		if (new Date().getTime() - progress.started > STATUS_TIMEOUT){
+			delete progress[id]
+		}
+	})
+}
+
+setInterval(() => {console.log(status); console.log("cleaning up..."); cleanUpStatus()} , CLEANUP_INTERVAL)
 
 app.use(express.json())
 
@@ -22,18 +40,45 @@ app.get('/', (req, res) => {
   	res.send('Beatport cart server!')
 })
 
+
+/*
+Gets the status of a job specified by the id
+**/
+app.get('/status/:id', (req, res) => {
+	try{
+		if (status[req.params.id].toProcess === 0){
+			res.send(JSON.stringify({status : status[req.params.id]}))
+			delete status[req.params.id]
+		}else{
+			res.send(JSON.stringify({status : status[req.params.id]}))
+		}
+	}catch(error){
+		res.status(500).send("Internal server error")
+	}
+	
+})
+
+
+/*
+Starts adding to cart process and responds letting the client know where to find updates
+
+searches : String separated by %20
+username : String
+password : String 
+**/
 app.post('/addToCart', async (req, res) => {
 
 	try{
-		const searches = req.body.searches.split("%20");
+		const searches = req.body.searches
 		const username = req.body.username;
 		const password = req.body.password;
-		const result = await controller.addToBeatportCart(username, password, searches)
-
-		res.send(JSON.stringify({badSearches : result}))
+		const statusId = uuidv4()
+		status[statusId] = {badSearches : [], toProcess : searches.length, started : new Date().getTime()}
+		controller.addToBeatportCart(username, password, searches, status[statusId])
+		res.send(JSON.stringify({statusId : statusId, status : status[statusId]}))
   		
 	}catch(error){
-		res.status(400).send(error)
+		res.status(500).send(error)
 	}
 
 	
